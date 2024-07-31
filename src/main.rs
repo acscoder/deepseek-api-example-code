17e2f73baed13e524fa3e8e43710aa297d924513
acscoder-digitalonda <acscoder@digitalonda.com>
2024-07-31 16:27:57 +0700

    up

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use deepseek_api_client::*;
 use futures_util::StreamExt;
 use tokio::runtime::Runtime;
+use std::env;
 
-static DEEPSEEK_API_KEY:&str = "sk-";
+use lazy_static::lazy_static;
+lazy_static! {
+    static ref DEEPSEEK_API_KEY: String = std::env::var("DEEPSEEK_API_KEY").unwrap();
+}
 #[tokio::main]
 async fn main()-> Result<(), Box<dyn std::error::Error>> {
     //call API in async function
@@ -16,14 +20,14 @@ async fn main()-> Result<(), Box<dyn std::error::Error>> {
                 content: "Write Hello world in rust".to_owned(),
             },
         ];
-        let mut llm = chat_completion(DEEPSEEK_API_KEY);
+        let mut llm = chat_completion(&DEEPSEEK_API_KEY);
         let res = llm(messages.clone()).await?;
         let res_text = get_response_text(&res, 0);
         println!("{:?}",res_text);
 
     
     //call API in Stream
-    let mut stream_llm = chat_completion_stream(DEEPSEEK_API_KEY);        
+    let mut stream_llm = chat_completion_stream(&DEEPSEEK_API_KEY);        
     let dt = stream_llm(messages.clone());
     let res = dt.await?;
     let mut stream = res.bytes_stream();
@@ -54,7 +58,7 @@ fn call_api_in_sync_function(){
                 content: "Write Hello world in rust".to_owned(),
             },
         ];
-    let mut sync_llm = chat_completion_sync(DEEPSEEK_API_KEY);
+    let mut sync_llm = chat_completion_sync(&DEEPSEEK_API_KEY);
     let res = sync_llm(messages);
     let res_text = get_response_text(&res.unwrap(), 0);
     println!("{:?}",res_text);
